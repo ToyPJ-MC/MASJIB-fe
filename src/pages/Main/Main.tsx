d78833c4ac89dd84aba77fae480b30c0befd8a09
Dark-jin <chch4lee1@naver.com>
2023-10-30 18:26:35 +0900

    feat: first page ui

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
 import restaurantimg from '../../assets/restaurant.jpg';
+import { Button, createTheme } from '@mui/material';
+
+let theme = createTheme({});
+theme = createTheme(theme, {
+  palette: {
+    white: theme.palette.augmentColor({
+      color: {
+        main: '#ffffff'
+      },
+      name: 'white'
+    })
+  }
+});
 
 const Main = () => {
   return (
@@ -12,9 +25,36 @@ const Main = () => {
         height: '100vh',
         width: '100vw'
       }}
-      className='z-0'
+      className='z-0 relative'
     >
-      <div className='text-lg text-white font-bold z-10'>Main</div>
+      <div className='z-10 text-white'>
+        <div className='grid grid-cols-3 ml-8 absolute bottom-6'>
+          <div>
+            <div className='text-6xl'>MASJIB</div>
+            <Button
+              variant='outlined'
+              size='large'
+              sx={{
+                color: 'white',
+                outlineColor: 'white',
+                border: 3,
+                ':hover': { borderColor: 'white', border: 3 },
+                marginTop: 3,
+                borderRadius: 30
+              }}
+            >
+              Review
+            </Button>
+          </div>
+          <div></div>
+          <div className='text-3xl'>
+            <div>This is a service to share your</div>
+            <div>own delicious restaurants.</div>
+            <div>We introduce various</div>
+            <div>restaurants.</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
