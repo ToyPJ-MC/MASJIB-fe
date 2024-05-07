f7dffcbaf4d2998f7042d2e0e1b57dc36c3d48df
Dark-jin <chch4lee1@naver.com>
2024-05-07 19:41:26 +0900

    fix: radius api 연결시 map 두개 생겨서 겹치는 현상

--- src/component/Kakaomap.tsx
+++ src/component/Kakaomap.tsx
@@ -102,6 +102,7 @@ const Kakaomap = () => {
   //   }
   // }, [currentlocation, lat, lon]);
 
+  // 반경 1km 음식점 마커 표시
   useEffect(() => {
     if (
       lat !== 0 &&
@@ -131,27 +132,6 @@ const Kakaomap = () => {
         position: locPosition,
         image: markerImage
       });
-      marker.setMap(map);
-      let zoomControl = new window.kakao.maps.ZoomControl();
-      map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
-      map.setCenter(new window.kakao.maps.LatLng(lat, lon));
-      if (currentlocation) {
-        map.setCenter(new window.kakao.maps.LatLng(lat, lon));
-        setCurrentlocation(false);
-      }
-    }
-  }, [lat, lon, currentlocation]);
-
-  useEffect(() => {
-    if (lat !== 0 && lon !== 0) {
-      // 현재 위치 마커 및 음식점 위치 마커 표시
-      let container = document.getElementById('map');
-      let options = {
-        center: new window.kakao.maps.LatLng(lat, lon),
-        level: 4
-      };
-      let map = new window.kakao.maps.Map(container, options);
-      let place = new window.kakao.maps.services.Places();
       if (search === 'Restaurant' && radiusMarker.length !== 0) {
         // 반경 내 음식점 마커 표시
         setLoading(true);
@@ -197,79 +177,101 @@ const Kakaomap = () => {
           });
         });
       }
-      // if (search !== 'Restaurant') {
-      //   place.keywordSearch(search, modalSearchCB);
-      //   function modalSearchCB(
-      //     // 음식점 검색 마커 표시
-      //     result: any,
-      //     status: any,
-      //     Pagination: any
-      //   ) {
-      //     if (status === window.kakao.maps.services.Status.OK) {
-      //       setLoading(true);
-      //       result.map((item: any) => {
-      //         if (item.category_group_code !== 'FD6') return;
-      //         setSearchResult((prev) => [
-      //           ...prev,
-      //           {
-      //             phone: item.phone,
-      //             place_name: item.place_name,
-      //             road_address_name: item.road_address_name
-      //           }
-      //         ]);
-      //       });
-      //       let bounds = new window.kakao.maps.LatLngBounds();
-      //       for (let i = 0; i < result.length; i++) {
-      //         if (result[i].category_group_code !== 'FD6') continue;
-      //         displayMarker(result[i]);
-      //         bounds.extend(
-      //           new window.kakao.maps.LatLng(result[i].y, result[i].x)
-      //         );
-      //       }
-      //       map.setBounds(bounds);
-      //     }
-      //     if (Pagination.hasNextPage) {
-      //       Pagination.nextPage(); // 다음 페이지로 요청
-      //     }
-      //   }
-      //   function displayMarker(place: any) {
-      //     let marker = new window.kakao.maps.Marker({
-      //       map: map,
-      //       position: new window.kakao.maps.LatLng(place.y, place.x)
-      //     });
-      //     let infowindow = new window.kakao.maps.InfoWindow({
-      //       content: place.place_name + '<br>' + place.road_address_name
-      //     });
-      //     window.kakao.maps.event.addListener(marker, 'click', function () {
-      //       window.open(
-      //         `/review?restaurantname=${place.place_name}&address=${place.road_address_name}&x=${place.x}&y=${place.y}`,
-      //         '_blank'
-      //       );
-      //     });
-      //     window.kakao.maps.event.addListener(marker, 'mouseover', function () {
-      //       infowindow.open(map, marker);
-      //     });
-      //     window.kakao.maps.event.addListener(marker, 'mouseout', function () {
-      //       infowindow.close();
-      //     });
-      //   }
-      // }
-      let marker = new window.kakao.maps.Marker({
-        // 현재 위치 마커
-        map: map,
-        position: locPosition,
-        image: markerImage
-      });
       marker.setMap(map);
       let zoomControl = new window.kakao.maps.ZoomControl();
       map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
+      map.setCenter(new window.kakao.maps.LatLng(lat, lon));
       if (currentlocation) {
         map.setCenter(new window.kakao.maps.LatLng(lat, lon));
         setCurrentlocation(false);
-        setSearch('Restaurant');
       }
     }
-  }, [search, radiusMarker]);
+  }, [lat, lon, currentlocation, radiusMarker]);
+
+  // useEffect(() => {
+  //   if (lat !== 0 && lon !== 0) {
+  //     // 현재 위치 마커 및 음식점 위치 마커 표시
+  //     let container = document.getElementById('map');
+  //     let options = {
+  //       center: new window.kakao.maps.LatLng(lat, lon),
+  //       level: 4
+  //     };
+  //     let map = new window.kakao.maps.Map(container, options);
+  //     let place = new window.kakao.maps.services.Places();
+
+  //     // if (search !== 'Restaurant') {
+  //     //   place.keywordSearch(search, modalSearchCB);
+  //     //   function modalSearchCB(
+  //     //     // 음식점 검색 마커 표시
+  //     //     result: any,
+  //     //     status: any,
+  //     //     Pagination: any
+  //     //   ) {
+  //     //     if (status === window.kakao.maps.services.Status.OK) {
+  //     //       setLoading(true);
+  //     //       result.map((item: any) => {
+  //     //         if (item.category_group_code !== 'FD6') return;
+  //     //         setSearchResult((prev) => [
+  //     //           ...prev,
+  //     //           {
+  //     //             phone: item.phone,
+  //     //             place_name: item.place_name,
+  //     //             road_address_name: item.road_address_name
+  //     //           }
+  //     //         ]);
+  //     //       });
+  //     //       let bounds = new window.kakao.maps.LatLngBounds();
+  //     //       for (let i = 0; i < result.length; i++) {
+  //     //         if (result[i].category_group_code !== 'FD6') continue;
+  //     //         displayMarker(result[i]);
+  //     //         bounds.extend(
+  //     //           new window.kakao.maps.LatLng(result[i].y, result[i].x)
+  //     //         );
+  //     //       }
+  //     //       map.setBounds(bounds);
+  //     //     }
+  //     //     if (Pagination.hasNextPage) {
+  //     //       Pagination.nextPage(); // 다음 페이지로 요청
+  //     //     }
+  //     //   }
+  //     //   function displayMarker(place: any) {
+  //     //     let marker = new window.kakao.maps.Marker({
+  //     //       map: map,
+  //     //       position: new window.kakao.maps.LatLng(place.y, place.x)
+  //     //     });
+  //     //     let infowindow = new window.kakao.maps.InfoWindow({
+  //     //       content: place.place_name + '<br>' + place.road_address_name
+  //     //     });
+  //     //     window.kakao.maps.event.addListener(marker, 'click', function () {
+  //     //       window.open(
+  //     //         `/review?restaurantname=${place.place_name}&address=${place.road_address_name}&x=${place.x}&y=${place.y}`,
+  //     //         '_blank'
+  //     //       );
+  //     //     });
+  //     //     window.kakao.maps.event.addListener(marker, 'mouseover', function () {
+  //     //       infowindow.open(map, marker);
+  //     //     });
+  //     //     window.kakao.maps.event.addListener(marker, 'mouseout', function () {
+  //     //       infowindow.close();
+  //     //     });
+  //     //   }
+  //     // }
+  //     let marker = new window.kakao.maps.Marker({
+  //       // 현재 위치 마커
+  //       map: map,
+  //       position: locPosition,
+  //       image: markerImage
+  //     });
+  //     marker.setMap(map);
+  //     let zoomControl = new window.kakao.maps.ZoomControl();
+  //     map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
+  //     if (currentlocation) {
+  //       map.setCenter(new window.kakao.maps.LatLng(lat, lon));
+  //       setCurrentlocation(false);
+  //       setSearch('Restaurant');
+  //     }
+  //   }
+  // }, [search, radiusMarker]);
   return (
     <>
       <div
@@ -318,7 +320,6 @@ const Kakaomap = () => {
             </Button>
           </>
         ) : null} */}
-
         {modal ? <SearchModal /> : null}
       </div>
     </>
