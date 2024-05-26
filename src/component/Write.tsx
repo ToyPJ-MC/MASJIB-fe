005cabedd1205ae4c8152ec13e4d7a4735d253a7
Dark-jin <chch4lee1@naver.com>
2024-05-26 11:55:21 +0900

    feat: 평점과 한줄평을 안쓰면 리뷰 작성 api block

--- src/component/Write.tsx
+++ src/component/Write.tsx
@@ -5,7 +5,7 @@ import {
   SideSheet,
   FileRejection
 } from 'evergreen-ui';
-import { useCallback, useMemo, useState } from 'react';
+import { FormEvent, useCallback, useMemo, useState } from 'react';
 import { writemodalState } from '../state/atom';
 import { useRecoilState } from 'recoil';
 import {
@@ -33,6 +33,7 @@ const Write = () => {
   const [fileRejections, setFileRejections] = useState<FileRejection[]>([]);
   const [onereview, setOneReview] = useState<string>('');
   const [review, setReview] = useState<string>('');
+  const [isSumbit, setIsSumbit] = useState<boolean>(false);
   const values = useMemo(
     () => [
       ...files,
@@ -64,22 +65,37 @@ const Write = () => {
   const useparam = new URL(location.href).searchParams;
   const shopid = Number(useparam.get('shopid'));
 
-  const reviewhandleSumbit = () => {
+  const reviewhandleSumbit = (e: FormEvent) => {
     //console.log(JSON.stringify(review)); // 리뷰 글 형식 그대로 전달
     //console.log(files); // 파일 형식 그대로 전달
-    const totalreview = onereview + '\n' + review;
-    if (getCookie('access_token') !== undefined) {
-      WriteReviewAPI(
-        totalreview,
-        shopid,
-        rating,
-        taste,
-        hygiene,
-        kindness,
-        files
-      );
+    e.preventDefault();
+    if (onereview.trim() === '') {
+      setIsSumbit(true);
+      toast.error('한줄평을 작성해주세요.');
+    }
+    if (rating === 0) {
+      setIsSumbit(true);
+      toast.error('평점을 선택해주세요.');
     } else {
-      toast.error('로그인이 필요합니다.');
+      setIsSumbit(false);
+      const totalreview = onereview + '\n' + review;
+      if (
+        getCookie('access_token') !== undefined &&
+        onereview.trim() !== '' &&
+        rating !== 0
+      ) {
+        WriteReviewAPI(
+          totalreview,
+          shopid,
+          rating,
+          taste,
+          hygiene,
+          kindness,
+          files
+        );
+      } else {
+        toast.error('로그인이 필요합니다.');
+      }
     }
   };
   //#endregion
@@ -91,199 +107,208 @@ const Write = () => {
         setOpen(false);
       }}
     >
-      <div className='grid place-items-center mt-4'>
-        <div className='text-lg font-semibold'>음식이 어땠나요?</div>
-        <Rating
-          name='half-rating'
-          defaultValue={0.0}
-          precision={0.5}
-          value={rating}
-          size='large'
-          onChange={(event, newValue) => {
-            setRating(newValue as number);
-          }}
-        />
-        <div className='grid grid-rows-3 place-items-center mt-4'>
-          <div className='grid place-items-center'>
-            <div className='font-semibold text-base'>맛은 어땠나요?</div>
-            <div className='grid grid-cols-2 place-items-center'>
-              <div>
-                <IconButton
-                  color='primary'
-                  onClick={() => {
-                    setTaste('good');
-                  }}
-                >
-                  {taste === 'good' ? (
-                    <ThumbUpIcon />
-                  ) : (
-                    <ThumbUpAltOutlinedIcon />
-                  )}
-                </IconButton>
-              </div>
-              <div>
-                <IconButton
-                  sx={{ color: '#FF4500' }}
-                  onClick={() => {
-                    setTaste('bad');
-                  }}
-                >
-                  {taste === 'bad' ? (
-                    <ThumbDownIcon />
-                  ) : (
-                    <ThumbDownAltOutlinedIcon />
-                  )}
-                </IconButton>
-              </div>
-            </div>
-          </div>
-          <div className='grid place-items-center'>
-            <div className='font-semibold text-base'>위생이 깨끗했나요?</div>
-            <div className='grid grid-cols-2 place-items-center'>
-              <div>
-                <IconButton
-                  color='primary'
-                  onClick={() => {
-                    setHygiene('good');
-                  }}
-                >
-                  {hygiene === 'good' ? (
-                    <ThumbUpIcon />
-                  ) : (
-                    <ThumbUpAltOutlinedIcon />
-                  )}
-                </IconButton>
-              </div>
-              <div>
-                <IconButton
-                  sx={{ color: '#FF4500' }}
-                  onClick={() => {
-                    setHygiene('bad');
-                  }}
-                >
-                  {hygiene === 'bad' ? (
-                    <ThumbDownIcon />
-                  ) : (
-                    <ThumbDownAltOutlinedIcon />
-                  )}
-                </IconButton>
-              </div>
-            </div>
-          </div>
-          <div className='grid place-items-center'>
-            <div className='font-semibold text-base'>
-              친절한 음직점이였나요?
-            </div>
-            <div className='grid grid-cols-2 place-items-center'>
-              <div>
-                <IconButton
-                  color='primary'
-                  onClick={() => {
-                    setKindness('good');
-                  }}
-                >
-                  {kindness === 'good' ? (
-                    <ThumbUpIcon />
-                  ) : (
-                    <ThumbUpAltOutlinedIcon />
-                  )}
-                </IconButton>
-              </div>
-              <div>
-                <IconButton
-                  sx={{ color: '#FF4500' }}
-                  onClick={() => {
-                    setKindness('bad');
-                  }}
-                >
-                  {kindness === 'bad' ? (
-                    <ThumbDownIcon />
-                  ) : (
-                    <ThumbDownAltOutlinedIcon />
-                  )}
-                </IconButton>
-              </div>
-            </div>
-          </div>
-        </div>
-        <FileUploader
-          acceptedMimeTypes={acceptedMimeTypes}
-          label='업로드 이미지'
-          description='최대 3개까지 업로드 가능합니다. 최대 10MB까지 업로드 가능합니다.'
-          disabled={files.length + fileRejections.length >= maxFiles}
-          maxSizeInBytes={maxSizeInBytes}
-          maxFiles={maxFiles}
-          onAccepted={(newFiles) =>
-            setFiles((prevFiles) => [...prevFiles, ...newFiles])
-          }
-          onRejected={(newFileRejections) =>
-            setFileRejections((prevFileRejections) => [
-              ...prevFileRejections,
-              ...newFileRejections
-            ])
-          }
-          renderFile={(file, index) => {
-            const { name, size, type } = file;
-            const renderFileCounterError =
-              index === 0 && fileCountOverLimit > 0;
-            const fileRejection = fileRejections.find(
-              (fileRejection: any) => fileRejection.file === file
-            );
-            const uniqueKey = `${file.name}_${index}`;
-            return (
-              <>
-                {renderFileCounterError && (
-                  <div className='grid place-items-center font-semibold text-red-500'>
-                    {fileCountError}
-                  </div>
-                )}
-                <FileCard
-                  key={uniqueKey}
-                  name={name}
-                  sizeInBytes={size}
-                  type={type}
-                  onRemove={() => handleRemove(file)}
-                  validationMessage={fileRejection?.message}
-                />
-              </>
-            );
-          }}
-          values={values}
-          className='mt-6'
-        />
-        <div className='grid place-items-center mt-2'>
-          <div className='text-base font-semibold'>음식점 리뷰</div>
-          <div className='mt-2 font-semibold'>한줄평</div>
-          <TextField
-            id='review'
-            placeholder='한줄평을 작성해주세요.'
-            onChange={(e) => {
-              setOneReview(e.target.value);
-            }}
-            className='w-96 outline outline-blue-500 rounded-lg mt-2'
-          />
-          <div className='mt-2 font-semibold'>리뷰</div>
-          <TextareaAutosize
-            name='review'
-            placeholder='리뷰를 작성해주세요.'
-            maxLength={250}
-            minRows={5}
-            maxRows={10}
-            className='w-96 h-fit outline outline-1 outline-neutral-300 rounded-lg mt-2 p-2'
-            onChange={(e) => {
-              setReview(e.target.value);
+      <form onSubmit={reviewhandleSumbit}>
+        <div className='grid place-items-center mt-4'>
+          <div className='text-lg font-semibold'>음식이 어땠나요?</div>
+          <Rating
+            name='half-rating'
+            defaultValue={0.0}
+            precision={0.5}
+            value={rating}
+            size='large'
+            onChange={(event, newValue) => {
+              setRating(newValue as number);
+              if (newValue !== 0) {
+                setIsSumbit(false);
+              }
             }}
           />
+          <div className='grid grid-rows-3 place-items-center mt-4'>
+            <div className='grid place-items-center'>
+              <div className='font-semibold text-base'>맛은 어땠나요?</div>
+              <div className='grid grid-cols-2 place-items-center'>
+                <div>
+                  <IconButton
+                    color='primary'
+                    onClick={() => {
+                      setTaste('good');
+                    }}
+                  >
+                    {taste === 'good' ? (
+                      <ThumbUpIcon />
+                    ) : (
+                      <ThumbUpAltOutlinedIcon />
+                    )}
+                  </IconButton>
+                </div>
+                <div>
+                  <IconButton
+                    sx={{ color: '#FF4500' }}
+                    onClick={() => {
+                      setTaste('bad');
+                    }}
+                  >
+                    {taste === 'bad' ? (
+                      <ThumbDownIcon />
+                    ) : (
+                      <ThumbDownAltOutlinedIcon />
+                    )}
+                  </IconButton>
+                </div>
+              </div>
+            </div>
+            <div className='grid place-items-center'>
+              <div className='font-semibold text-base'>위생이 깨끗했나요?</div>
+              <div className='grid grid-cols-2 place-items-center'>
+                <div>
+                  <IconButton
+                    color='primary'
+                    onClick={() => {
+                      setHygiene('good');
+                    }}
+                  >
+                    {hygiene === 'good' ? (
+                      <ThumbUpIcon />
+                    ) : (
+                      <ThumbUpAltOutlinedIcon />
+                    )}
+                  </IconButton>
+                </div>
+                <div>
+                  <IconButton
+                    sx={{ color: '#FF4500' }}
+                    onClick={() => {
+                      setHygiene('bad');
+                    }}
+                  >
+                    {hygiene === 'bad' ? (
+                      <ThumbDownIcon />
+                    ) : (
+                      <ThumbDownAltOutlinedIcon />
+                    )}
+                  </IconButton>
+                </div>
+              </div>
+            </div>
+            <div className='grid place-items-center'>
+              <div className='font-semibold text-base'>
+                친절한 음직점이였나요?
+              </div>
+              <div className='grid grid-cols-2 place-items-center'>
+                <div>
+                  <IconButton
+                    color='primary'
+                    onClick={() => {
+                      setKindness('good');
+                    }}
+                  >
+                    {kindness === 'good' ? (
+                      <ThumbUpIcon />
+                    ) : (
+                      <ThumbUpAltOutlinedIcon />
+                    )}
+                  </IconButton>
+                </div>
+                <div>
+                  <IconButton
+                    sx={{ color: '#FF4500' }}
+                    onClick={() => {
+                      setKindness('bad');
+                    }}
+                  >
+                    {kindness === 'bad' ? (
+                      <ThumbDownIcon />
+                    ) : (
+                      <ThumbDownAltOutlinedIcon />
+                    )}
+                  </IconButton>
+                </div>
+              </div>
+            </div>
+          </div>
+          <FileUploader
+            acceptedMimeTypes={acceptedMimeTypes}
+            label='업로드 이미지'
+            description='최대 3개까지 업로드 가능합니다. 최대 10MB까지 업로드 가능합니다.'
+            disabled={files.length + fileRejections.length >= maxFiles}
+            maxSizeInBytes={maxSizeInBytes}
+            maxFiles={maxFiles}
+            onAccepted={(newFiles) =>
+              setFiles((prevFiles) => [...prevFiles, ...newFiles])
+            }
+            onRejected={(newFileRejections) =>
+              setFileRejections((prevFileRejections) => [
+                ...prevFileRejections,
+                ...newFileRejections
+              ])
+            }
+            renderFile={(file, index) => {
+              const { name, size, type } = file;
+              const renderFileCounterError =
+                index === 0 && fileCountOverLimit > 0;
+              const fileRejection = fileRejections.find(
+                (fileRejection: any) => fileRejection.file === file
+              );
+              const uniqueKey = `${file.name}_${index}`;
+              return (
+                <>
+                  {renderFileCounterError && (
+                    <div className='grid place-items-center font-semibold text-red-500'>
+                      {fileCountError}
+                    </div>
+                  )}
+                  <FileCard
+                    key={uniqueKey}
+                    name={name}
+                    sizeInBytes={size}
+                    type={type}
+                    onRemove={() => handleRemove(file)}
+                    validationMessage={fileRejection?.message}
+                  />
+                </>
+              );
+            }}
+            values={values}
+            className='mt-6'
+          />
+          <div className='grid place-items-center mt-2'>
+            <div className='text-base font-semibold'>음식점 리뷰</div>
+            <div className='mt-2 font-semibold'>한줄평</div>
+            <TextField
+              id='review'
+              placeholder='한줄평을 작성해주세요.'
+              onChange={(e) => {
+                setOneReview(e.target.value);
+                if (e.target.value.trim() !== '') {
+                  setIsSumbit(false);
+                }
+              }}
+              className='w-96 outline outline-blue-500 rounded-lg mt-2'
+            />
+            <div className='mt-2 font-semibold'>리뷰</div>
+            <TextareaAutosize
+              name='review'
+              placeholder='리뷰를 작성해주세요.'
+              maxLength={250}
+              minRows={5}
+              maxRows={10}
+              className='w-96 h-fit outline outline-1 outline-neutral-300 rounded-lg mt-2 p-2'
+              onChange={(e) => {
+                setReview(e.target.value);
+              }}
+            />
+          </div>
+          <Button
+            variant='contained'
+            sx={{ marginTop: '10px' }}
+            size='large'
+            onClick={reviewhandleSumbit}
+            //disabled={onereview.trim() === '' || rating === 0}
+          >
+            리뷰 쓰기
+          </Button>
         </div>
-        <Button
-          variant='contained'
-          sx={{ marginTop: '10px' }}
-          size='large'
-          onClick={reviewhandleSumbit}
-        >
-          리뷰 쓰기
-        </Button>
-      </div>
+      </form>
     </SideSheet>
   );
 };
